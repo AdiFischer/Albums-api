bfda650495ba91bc6643b682b57fca8dcb2a8fc9
AdiFischer <adifischer1@gmail.com>
2022-11-01 13:49:06 -0400

    more changes

---
+++ albums-react/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import AlbumList from './components/AlbumsList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AlbumList />
+    </div>
+  );
+}
+
+export default App;
